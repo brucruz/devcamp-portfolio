7ee5890d41a0b5e23c54703123a3933c7be463dd
Bruno <bru.cruz@gmail.com>
2017-11-24 18:32:38 -0200

    integrated index action for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,5 @@
-class PorfoliosController < ApplicationController
+class PortfoliosController < ApplicationController
+  def index
+    @portfolio_items = Portfolio.all
+  end
 end
--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,2 @@
-module PorfoliosHelper
+module PortfoliosHelper
 end
