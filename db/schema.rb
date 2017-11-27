fb69f7097a4ce670efefd8e2a1ea8d9494e4c5e3
Bruno <bru.cruz@gmail.com>
2017-11-27 20:06:57 -0200

    implemented concern to manage placeholder data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127202415) do
+ActiveRecord::Schema.define(version: 20171127212014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20171127202415) do
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
